7056a49f7be83d81debd35419689fef5ace3a812
heitor0803 <hcpalmeira210@gmail.com>
2025-10-01 10:35:24 -0300

    main.site

--- site/script.js
+++ site/script.js
@@ -3,5 +3,5 @@ const imagem = document.getElementById('Logo html')
 // 2. Adiciona um "ouvinte de evento" para o clique
 imagem.addEventListener('click', function() {
     // 3. Quando o clique ocorrer, redireciona para a URL desejada
-    window.location.href = 'https://pt.wikipedia.org/wiki/HTML';
+    window.location.href = 'https://r2pbconfeccoes.com.br/?srsltid=AfmBOoprAsi2L0jMU-mV5QBOiIvq-FY-AfTGFiysNqwLYdd_xfpLbvCr';
 });
