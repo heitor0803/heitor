8a9f8754e1dfee900d0e85d3ea8d748b26f8c12a
heitor0803 <hcpalmeira210@gmail.com>
2025-10-02 08:55:36 -0300

    main.site

--- site/script.js
+++ site/script.js
@@ -5,3 +5,19 @@ imagem.addEventListener('click', function() {
     // 3. Quando o clique ocorrer, redireciona para a URL desejada
     window.location.href = 'https://r2pbconfeccoes.com.br/?srsltid=AfmBOoprAsi2L0jMU-mV5QBOiIvq-FY-AfTGFiysNqwLYdd_xfpLbvCr';
 });
+
+// 1. Pega o elemento da imagem usando o ID
+const imagem = document.getElementById('Whatsapp')
+// 2. Adiciona um "ouvinte de evento" para o clique
+imagem.addEventListener('click', function() {
+    // 3. Quando o clique ocorrer, redireciona para a URL desejada
+    window.location.href = 'https://web.whatsapp.com';
+});
+
+// 1. Pega o elemento da imagem usando o ID
+const imagem = document.getElementById('instagram')
+// 2. Adiciona um "ouvinte de evento" para o clique
+imagem.addEventListener('click', function() {
+    // 3. Quando o clique ocorrer, redireciona para a URL desejada
+    window.location.href = 'https://www.instagram.com/r2pbfabricaderoupas/?hl=pt';
+});
