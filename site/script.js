50e6a3ef890b798184e509d31642e6bab291dc99
heitor0803 <hcpalmeira210@gmail.com>
2025-10-02 10:52:24 -0300

    main.site

--- site/script.js
+++ site/script.js
@@ -1,23 +1,23 @@
 // 1. Pega o elemento da imagem usando o ID
-const imagem = document.getElementById('Logo html')
+const r2pb = document.getElementById('Logo html')
 // 2. Adiciona um "ouvinte de evento" para o clique
-imagem.addEventListener('click', function() {
+r2pb.addEventListener('click', function() {
     // 3. Quando o clique ocorrer, redireciona para a URL desejada
     window.location.href = 'https://r2pbconfeccoes.com.br/?srsltid=AfmBOoprAsi2L0jMU-mV5QBOiIvq-FY-AfTGFiysNqwLYdd_xfpLbvCr';
 });
 
 // 1. Pega o elemento da imagem usando o ID
-const imagem = document.getElementById('Whatsapp')
+const whatsapp=document.getElementById('Whatsapp')
 // 2. Adiciona um "ouvinte de evento" para o clique
-imagem.addEventListener('click', function() {
+whatsapp.addEventListener('click', function() {
     // 3. Quando o clique ocorrer, redireciona para a URL desejada
     window.location.href = 'https://web.whatsapp.com';
 });
 
 // 1. Pega o elemento da imagem usando o ID
-const imagem = document.getElementById('instagram')
+const instagram = document.getElementById('instagram')
 // 2. Adiciona um "ouvinte de evento" para o clique
-imagem.addEventListener('click', function() {
+instagram.addEventListener('click', function() {
     // 3. Quando o clique ocorrer, redireciona para a URL desejada
     window.location.href = 'https://www.instagram.com/r2pbfabricaderoupas/?hl=pt';
 });
