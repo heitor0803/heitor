ab757015e20db824c276f783747a3cff44712e15
heitor0803 <hcpalmeira210@gmail.com>
2025-10-30 09:54:14 -0300

    z
    Added commit to save changes to the database after inserting a record.

--- Mini_armazenador_de_dados/z.py
+++ Mini_armazenador_de_dados/z.py
@@ -6,12 +6,10 @@ cursor = banco.cursor()
 
 #cursor.execute("CREATE TABLE pessoas (nome text,idade integer,email text)")
 
-""" pessoa= input('qual seu nome')
+pessoa= input('qual seu nome')
 idade = int(input('qual sua idade'))
 gmail = input('qual seu gmail')
 
 cursor.execute("INSERT INTO pessoas VALUES(?,?,?)",(pessoa,idade,gmail))
- """
-cursor.execute("SELECT * FROM pessoas")
-print(cursor.fetchall())
+
 banco.commit()
