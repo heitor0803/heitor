a1acc61438ce6437a6759318e52afdad472533db
heitor0803 <hcpalmeira210@gmail.com>
2025-10-31 11:48:40 -0300

    Update z.py

--- Mini_armazenador_de_dados/z.py
+++ Mini_armazenador_de_dados/z.py
@@ -1,11 +1,1 @@
-import sqlite3 as sq
-
-banco= sq.connect('primeiro_banco.db')
-
-cursor = banco.cursor()
-
-cursor.execute("CREATE TABLE pessoas (nome text,idade integer,email text)")
-
-banco.commit()
-
 
